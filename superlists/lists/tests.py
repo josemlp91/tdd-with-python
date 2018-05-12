b491dc9b62b2d1c78f57cbe173ec7d37bdc80391
Jose Miguel <josmilope@gmail.com>
2018-05-12 19:47:30 +0200

    testing with models

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,8 +3,9 @@ from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import *
+from lists.models import *
 
-# Create your tests here.
+
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         """La ruta raiz tiene una vista asociada."""
@@ -26,3 +27,34 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
         self.assertIn('A new list item', response.content.decode())
+
+
+    def test_home_page_can_save_a_POST_request_and_render(self):
+        """El formulario principal lee datos del usuario y los representa en la vista HTML"""
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+        
+        expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
+        self.assertEqual(response.content.decode(), expected_html)
+
+
+class ItemModelTest(TestCase):
+    
+    def test_saving_and_retrieving_items(self):
+        """ El sistema almacena correctamente objetos del todo"""
+        first_item = Item()
+        first_item.text = 'La primera cosa de la lista'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'La segunda cosa'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        self.assertEqual(first_saved_item.text, first_item.text )
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from django.views.decorators.csrf import csrf_exempt
+
 # Create your views here.
+
+@csrf_exempt
 def home_page(request):
+
+    if request.method == 'POST':
+        return render(request, "home.html", {'new_item_text': request.POST.get('item_text')})
     return render(request, "home.html")
