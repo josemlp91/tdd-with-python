0ccf57cb5731fd7c5c54b8b2788141513350792b
Jose Miguel <josmilope@gmail.com>
2018-05-12 18:51:52 +0200

    test first view

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,12 +1,19 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
 from lists.views import *
 
 # Create your tests here.
 class HomePageTest(TestCase):
-    def test_root_yrl_resolves_to_home_page_view(self):
-        """
-        Ejemplo
-        """
+    def test_root_url_resolves_to_home_page_view(self):
+        """La ruta raiz tiene una vista asociada."""
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        """La pagina principal renderiza un contenido HTML válido."""
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
