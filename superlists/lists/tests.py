b099c021f801fe0bc420f2bdc4bffa09b5ab4427
Jose Miguel <josmilope@gmail.com>
2018-05-12 18:57:05 +0200

    refactoring test

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from lists.views import *
 
 # Create your tests here.
@@ -14,6 +15,6 @@ class HomePageTest(TestCase):
         """La pagina principal renderiza un contenido HTML válido."""
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html = render_to_string("home.html")
+        self.assertEqual(response.content.decode(), expected_html)
+        
