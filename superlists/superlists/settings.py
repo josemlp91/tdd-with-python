c9977149a2978235fab72f657da646cd1f8832f4
Jose Miguel <josmilope@gmail.com>
2018-05-13 13:22:54 +0200

    config heroku

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -128,3 +130,6 @@ STATIC_URL = '/static/'
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 NOSE_ARGS = ['--with-spec', '--spec-color']
+
+
+django_heroku.settings(locals())
