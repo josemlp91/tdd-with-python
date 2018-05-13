192a02a8c7984f6a33246df7384e76d3a16056f3
Jose Miguel <josmilope@gmail.com>
2018-05-13 14:42:46 +0200

    prueba

--- lists/tests.py
+++ lists/tests.py
@@ -40,21 +40,3 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html', {'new_item_text': 'A new list item'})
         self.assertEqual(response.content.decode(), expected_html)
 
-
-class ItemModelTest(TestCase):
-    
-    def test_saving_and_retrieving_items(self):
-        """ El sistema almacena correctamente objetos del todo"""
-        first_item = Item()
-        first_item.text = 'La primera cosa de la lista'
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'La segunda cosa'
-        second_item.save()
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        self.assertEqual(first_saved_item.text, first_item.text )
